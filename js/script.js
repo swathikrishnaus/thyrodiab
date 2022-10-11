bd788b3b4e420845128ea966dfecca56d0f787ef
swathikrishnaus <swathikrishnaus2015@gmail.com>
2022-10-11 11:07:20 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -5,3 +5,22 @@ $(document).scroll(function () {
     else
         $(".navbar").removeClass("nav-shadow")
 })
+
+
+
+
+
+
+
+
+// ----------------------filter gallery----------------------------
+
+$(".book-now-btn").click(function () {
+    var divID = $(this).attr("btn-for");
+    $(".place").addClass("d-none");
+    $("#" + divID).removeClass("d-none");
+    $(".book-now-btn").removeClass("active");
+    $(this).addClass("active");
+})
+
+// ----------------------filter gallery----------------------------
